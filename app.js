b6f081f535bd2e71fe09bbb5e993b8c6b18ff635
Maria Dominique <mddaculan@gmail.com>
2021-01-05 22:16:21 +0800

    files for admin modules-incomplete

--- app.js
+++ app.js
@@ -19,8 +19,21 @@ app.use("/",landing);
 const gallery = require("./public/router/gallery.js");
 app.use("/gallery",gallery);
 
+// const admin_gallery = require("./public/router/admin_gallery.js");
+// app.use("/gallery",admin_gallery);
+/* di ra cguro ni needed...naa lay features mahidden/disable depende sa user-type*/
+
 const login = require("./public/router/clientlogin.js");
 app.use("/clientlogin",login);
+
+const artist = require("./public/router/artist_records.js");
+app.use("/artist",artist);
+
+const client = require("./public/router/client_records.js");
+app.use("/client",client);
+
+const project = require("./public/router/project_records.js");
+app.use("/project",project);
 
 app.listen(4000,"0.0.0.0",()=>{
     console.log("Connected to port 4000");
