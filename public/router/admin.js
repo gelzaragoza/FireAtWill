8639e12580533e11c98c7572b8e6bde03e54f187
Maria Dominique <mddaculan@gmail.com>
2021-01-09 14:44:51 +0800

    corrected navbar

--- public/router/admin.js
+++ public/router/admin.js
@@ -22,7 +22,7 @@ router.get("/client",(req,res)=>{
     res.render("admin/client_records");
 })
 
-router.get("/projects",(req,res)=>{
+router.get("/project_records",(req,res)=>{
     res.render("admin/project_records");
 })
 
