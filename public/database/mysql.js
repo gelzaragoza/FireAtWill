bc77dd6cd1568725ec8ace8036c5e08d06dba5b2
Angelica <16101416@usc.edu.ph>
2021-01-15 23:33:14 +0800

    dashboard minor changes

--- public/database/mysql.js
+++ public/database/mysql.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "",
-    database: "newtattoo_db",
+    database: "tattoo_db",
     multipleStatements: true
 });
 connection.connect((err)=>{
