a668568ba4bdfbe16a5b4a5476671ee52bd0ab1f
Jayson Cadiz <cadizjayson25@yahoo.com>
2021-01-19 22:02:56 +0800

    Client UI Updated

--- public/database/mysql.js
+++ public/database/mysql.js
@@ -1,5 +1,4 @@
 const mysql = require("mysql");
-const { connect } = require("../router/admin");
 const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
@@ -12,14 +11,41 @@ connection.connect((err)=>{
     console.log("Database Connected");
 })
 module.exports = {
-    addApointments: function(body,callback){
-        connection.query("SELECT * FROM appointment; SELECT * FROM client",(err,table)=>{
-            console.log(table[0]);
-            callback();
-        })
+    addApointments: function(body,id,callback){
+        if(body.imglink==""&&body.imgarc==undefined){
+            connection.query("INSERT INTO appointment(Client_id,Date_Created,Appointment_Date,Image_Submission,Image_Archive_ID,purpose,Status) VALUES("+id+",CURDATE(),'"+body.date+"','N/A',0,'"+body.purpose+"','Pending')",(err,res)=>{
+                if(err) throw(err);
+                callback();
+            })
+        }else if(body.imgarc==undefined){
+            connection.query("INSERT INTO appointment(Client_id,Date_Created,Appointment_Date,Image_Submission,Image_Archive_ID,purpose,Status) VALUES("+id+",CURDATE(),'"+body.date+"','"+body.imglink+"',0,'"+body.purpose+"','Pending')",(err,res)=>{
+                if(err) throw(err);
+                callback();
+            })
+        }else{
+            connection.query("INSERT INTO appointment(Client_id,Date_Created,Appointment_Date,Image_Submission,Image_Archive_ID,purpose,Status) VALUES("+id+",CURDATE(),'"+body.date+"','N/A',1,'"+body.purpose+"','Pending')",(err,res)=>{
+                if(err) throw(err);
+                callback();
+            })
+        }
+    },
+    getAppointments: function(id,callback){
+        connection.query("SELECT * FROM appointment WHERE Client_ID="+id,(err,app)=>{
+            if(err) throw(err);
+            callback(app);
+        });
     },
     addSession: function(){
 
+    },
+    loginClient: function(contact,callback){
+        connection.query("SELECT * FROM client WHERE Contact_Number='"+contact+"'",(err,client)=>{
+            if(client.length==1){
+                callback(client);
+            }else{
+                callback(null);
+            }
+        });
     }
 }
 /*
--- public/router/clientlogin.js
+++ public/router/clientlogin.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
-
+const mysql = require("../database/mysql.js");
 
 router.get("/",(req,res)=>{
     res.render("client/clientlogin");
+})
+
+router.post("/",(req,res)=>{
+    mysql.loginClient(req.body.number,(client)=>{
+        if(client!=null){
+            req.session.userid=client[0].Client_ID;
+            req.session.type="client";
+            req.session.user= client[0].First_Name+" "+client[0].Last_Name;
+            res.redirect("/client")
+        }else{
+            res.redirect("/clientlogin")
+        }
+    });
 })
 
 module.exports= router;
