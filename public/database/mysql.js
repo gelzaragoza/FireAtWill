eaf42c9479dd27edd85716e3a98a8a07797d6aeb
Jayson Cadiz <cadizjayson25@yahoo.com>
2021-01-22 16:28:16 +0800

    minor

--- public/database/mysql.js
+++ public/database/mysql.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "",
-    database: "tattoo_db",
+    database: "newtattoo_db",
     multipleStatements: true
 });
 connection.connect((err)=>{
